f2b2329f05d7fc136c951bccc666d2b8ac618b6c
Eady <edith.yelf@sky.uk>
2020-06-05 17:46:37 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Lightning, Utils } from 'wpe-lightning-sdk';
+
+export default class App extends Lightning.Component {
+  static getFonts() {
+    return [{ family: 'pixel', url: Utils.asset('fonts/pixel.ttf'), descriptor: {} }]
+  }
+  static _template() {
+    return {
+      rect: true,
+      color: 0xff000000,
+      w: 1920,
+      h: 1080,
+    }
+  }
+  static _states() {
+    return []
+  }
+}
